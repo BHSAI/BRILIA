9cbcfbdb6d3269e9aba4d8c30c815e764724c98e
dleebhsai <dlee@bhsai.org>
2017-01-20 17:37:12 -0500

    Add files via upload

--- Database_Manager/getCurrentDatabase.m
+++ Database_Manager/getCurrentDatabase.m
@@ -13,7 +13,14 @@
 %  [...] = getCurrentDatabase('change') will prompt user to select a
 %  database from the active or dormant folder. Once selected, the database
 %  will move the active folder, while the others move the dormant folder.
-
+%
+%  [...] = getCurrentDatabase('change',Host) will select the database for
+%  the host species, and then set it to the active database.
+%    Host = 'human'
+%    Host = 'mouse'
+%    Host = [Exact name of the database file, EX:
+%    IMGT_Human_VDJ_database.mat].
+%
 function varargout = getCurrentDatabase(varargin)
 %Find the directory for the active and dormant database *.mat files
 Mpath = mfilename('fullpath');
@@ -34,17 +41,18 @@ else
     SelectDatabase = 0;
 end
 
-%If use specifiy to change database, look for database
+%If user specifiy to change database, look for database
 if ~isempty(varargin)
     if strcmpi(varargin{1},'change')
         SelectDatabase = 1;
-        SelectThisOne = ''; %If you know the exact file, select this
+        if nargin < 2
+            SelectThisOne = ''; %If you know the exact file, select this
+        else
+            SelectThisOne = varargin{2};
+        end
 
         %Acknowledge IMGT database references
         sprintf('Germline VDJ databases were downloaded from the IMGT, the international ImMunoGeneTics information system \n at http://www.imgt.org (founder and director: Marie-Paule Lefranc, Montpellier, France)')
-    else
-        SelectDatabase = 1;
-        SelectThisOne = varargin{1};
     end
 end
 
@@ -54,12 +62,12 @@ if SelectDatabase == 1
     while 1
         if isempty(SelectThisOne)
             if ~isempty(ActiveMat)
-                disp(['0 or Enter) ' ActiveMat(1).name]);
+                disp(['Enter) ' ActiveMat(1).name]);
             end
             for j = 1:length(DormantMat)
-                disp([num2str(j) ') ' DormantMat(j).name]);
+                disp([num2str(j) '    ) ' DormantMat(j).name]);
             end
-            SelectNum = input('Select the database number to use');
+            SelectNum = input('Select the database number to use: ');
             if ~isempty(SelectNum)
                 if SelectNum > 0 && SelectNum <= length(DormantMat)
                     disp(['Using: ' DormantMat(SelectNum).name]);
@@ -75,13 +83,13 @@ if SelectDatabase == 1
                 break            
             end
         else %You know what database to use, select this one.
-            if ~isempty(strfind(ActiveMat(1).name,SelectThisOne))
+            if ~isempty(regexpi(ActiveMat(1).name,SelectThisOne))
                 SelectNum = 0;
                 break;
             else
                 SelectNum = 0; %Just in case  you can't find one.
                 for j = 1:length(DormantMat)
-                    if strfind(DormantMat(j).name,SelectThisOne)
+                    if ~isempty(regexpi(DormantMat(j).name,SelectThisOne))
                        SelectNum = j;
                        break
                     end
---
+++ Database_Manager/renumberVDJmap.m
@@ -0,0 +1,42 @@
+%renumberVDJmap will take renumber all V_MapNum, D_MapNum, and J_MapNum
+%based on the full gene name. Use this when changing/updating databases.
+%
+%  VDJdata = renumberVDJmap(VDJdata,NewHeader)
+%
+%  VDJdata = renumberVDJmap(VDJdata,NewHeader,Vmap,Dmap,Jmap)
+
+function VDJdata = renumberVDJmap(VDJdata,NewHeader,varargin)
+%Extract the VDJ database
+if length(varargin) == 3
+    [Vmap,Dmap,Jmap] = deal(varargin{:});
+else
+    [Vmap, Dmap, Jmap] = getCurrentDatabase;
+end
+getHeaderVar;
+
+%Renumber the V, D, J reference gene map number
+for j = 1:size(VDJdata,1)
+    for q = 1:3
+        %Extract the gene name(s)
+        Xfam = VDJdata{j,FamLoc(q)};
+        if sum(isempty(Xfam))>0 || sum(isnan(Xfam)) > 0; 
+            continue
+        end
+        
+        %Remove the general name (ex: "IGHV01:")
+        ColonLoc = regexp(Xfam,':');
+        if ~isempty(ColonLoc)
+            Xfam = Xfam(ColonLoc+1:end);
+            Xfam = strrep(Xfam,' ','');
+        end
+        
+        %Get the individual gene names, and reassign the ref number
+        XfamAll = regexp(Xfam,'\|','split');
+        XmapNum = zeros(1,length(XfamAll));
+        for v = 1:length(XfamAll)
+            [~, XmapNum(v)] = lookupVDJgene(XfamAll{v},Vmap,Dmap,Jmap);
+        end
+        
+        VDJdata{j,FamNumLoc(q)} = XmapNum;
+    end
+end
