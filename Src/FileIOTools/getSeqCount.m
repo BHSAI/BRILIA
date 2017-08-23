c57f8faf218d615517313f42ef9bc11e6860eebb
dleebhsai <dlee@bhsai.org>
2017-08-23 16:15:44 -0400

    Replaced matlab fastqinfo and fastainfo due to library access errors in 2017a. Now uses a simple fopen(filename, 'r') to count the number of sequences in the file.

--- Src/FileIOTools/getSeqCount.m
+++ Src/FileIOTools/getSeqCount.m
@@ -1,22 +1,41 @@
 %Returns the number of entries in a sequence files
-function SeqCount = getSeqCount(FullFileName)
+function SeqCount = getSeqCount(varargin)
 %Determine the file type here
-if isempty(FullFileName)
-    [InFileName, InFilePath] = uigetfile('*.fa*;*.csv', 'Select the input sequence file', 'MultiSelect', 'off');
-    if isempty(InFileName);
+if isempty(varargin)
+    [InFileName, InFilePath] = uigetfile('*.fa*;*.*sv', 'Select the input sequence file', 'MultiSelect', 'off');
+    if isempty(InFileName)
         error('getSeqCount: No file was selected.');
     end
     FullFileName = [InFilePath InFileName];
+else
+    FullFileName = varargin{1};
+    if ~exist(FullFileName, 'file')
+        error('%s: Could not fine file [ %s ].', mfilename, FullFileName);
+    end
 end
 [~, ~, InFileExt] = parseFileName(FullFileName);    
 
 %Check to see if there is a file type override
 if strcmpi(InFileExt, '.fa') || strcmpi(InFileExt, '.fasta')
-    Info = fastainfo(FullFileName);
-    SeqCount = Info.NumberOfEntries;
+    FID = fopen(FullFileName, 'r');
+    A = fgetl(FID);
+    SeqCount = 0;
+    while ischar(A)
+        if ~isempty(A) && A(1) == '>'
+            SeqCount = SeqCount + 1;
+        end
+        A = fgetl(FID);
+    end
 elseif strcmpi(InFileExt, '.fastq')
-    Info = fastqinfo(FullFileName);
-    SeqCount = Info.NumberOfEntries;
+    FID = fopen(FullFileName, 'r');
+    A = fgetl(FID);
+    SeqCount = 0;
+    while ischar(A)
+        if ~isempty(A) && A(1) == '@'
+            SeqCount = SeqCount + 1;
+        end
+        A = fgetl(FID);
+    end
 elseif strcmpi(InFileExt, '.csv') || strcmpi(InFileExt, '.tsv')
     FID = fopen(FullFileName, 'r');
     A = fgetl(FID);
