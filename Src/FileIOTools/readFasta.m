a04094e25fa2cb36103717c8c1e2d5a439f69b7f
dleebhsai <dlee@bhsai.org>
2017-09-12 10:34:16 -0400

    Faster fasta reader.

--- Src/FileIOTools/readFasta.m
+++ Src/FileIOTools/readFasta.m
@@ -1,99 +1,68 @@
 %readFasta will read a fasta file and return 2 cells containing the
-%sequence name and the sequence itself. [Not used as MATLAB has the
-%fastaread function]
+%sequence name and the sequence itself. Similar to matlab "fastaread"
+%but can handle files with empty line spacers.
 %
-%  [Header, Sequence] = readFasta(FileName);
+%  [Header, Seq] = readFasta();
 %
-%  [Header, Sequence] = readFasta();
+%  [Header, Seq] = readFasta(FileName);
+%
+%  [Header, Seq] = readFasta(FileName, 'SeqRange', Range)
 %
 %  INPUT
-%    FileName: full file name of the fasta file. If empty, will ask user to
-%    select the file.
+%    FileName: file name of the fasta file. If empty, will ask user to
+%      select the file.
+%    Range: 2-element integer vector specifying first and last sequence
+%      to read.
 %
 %  OUTPUT
 %    Header: Nx1 cell of sequence header information
-%    Sequence: Nx1 cell of sequences
+%    Seq: Nx1 cell of sequences
 
-function [Header, Sequence] = readFasta(varargin)
+function [Header, Seq] = readFasta(varargin)
 SeqRange = [1 Inf];
-for j = 1:length(varargin)-1
-    if ischar(varargin{j}) && strcmpi(varargin{j},'seqrange') || strcmpi(varargin{j},'blockread')
-        SeqRange = varargin{j+1};
-        varargin(j:j+1) = [];
+RangeOptLoc = strcmpi(varargin, 'seqrange') | strcmpi(varargin, 'blockread');
+if any(RangeOptLoc)
+    RangeIdx = find(RangeOptLoc) + 1;
+    SeqRange = varargin{RangeIdx(1)};
+    if length(SeqRange) == 1
+        SeqRange = SeqRange * [1 1];
     end
+    SeqRange(SeqRange < 1) = 1;
+    varargin(RangeIdx(1)-1:end) = [];
 end
 
-%Get the input file name
 if isempty(varargin)
-    [FileName, FilePath] = uigetfile('*.txt;*.fa*','Open FASTA file');
+    [FileName, FilePath] = uigetfile('*.txt;*.fa*', 'Open FASTA file');
     InputFileName = [FilePath FileName];
-elseif ischar(varargin{1})
+elseif exist(varargin{1}, 'file') || exist(fullfile(pwd, varargin{1}), 'file')
     InputFileName = varargin{1};
 else
-    error('readFasta: input file not specified correctly');
+    error('%s: input file not specified correctly.', mfilename);
 end
 
-% fprintf('%s: Fasta FileName: %s\n', mfilename, InputFileName);
-% fprintf('%s: SeqRange: %d to %d\n', mfilename, SeqRange(1), SeqRange(2));
-
-%Determine number of sequences first
-FID = fopen(InputFileName,'r');
+[FID, MSG] = fopen(InputFileName, 'r');
 if FID < 0
-    error('%s: Could not open FASTA file, %s', mfilename, InputFileName);
+    error('%s: Could not open FASTA file "%s".\n  %s', mfilename, InputFileName, MSG);
 end
-NumLines = 0;
-while 1
-    TextLine= fgetl(FID);
-    if ischar(TextLine) 
-        if ~isempty(TextLine) && strcmpi(TextLine(1), '>')
-            NumLines = NumLines+1;
-        end
-    else
-        break
-    end
-end
-fseek(FID,0,'bof');
-%fprintf('%s: Num of seq: %d\n', mfilename, NumLines);
-
-if SeqRange(1) < 1; SeqRange(1) = 1; end
-if SeqRange(2) > NumLines; SeqRange(2) = NumLines; end
-
-%Start the matrix generation
-Header = cell(diff(SeqRange) + 1,1);
-Sequence = cell(diff(SeqRange) + 1,1);
-j = 1;
-CurLine = 0;
-while CurLine < SeqRange(1)
-    TextLine= fgetl(FID);
-    if ischar(TextLine) 
-        if ~isempty(TextLine) && strcmpi(TextLine(1), '>')
-            CurLine = CurLine + 1;
-        end
-    else
-        break
-    end
-end
-if CurLine == 0
-    return;
-end
-while CurLine <= SeqRange(2)
-    if ~isempty(TextLine) && strcmp(TextLine(1),'>') %Get the header
-        Header{j} = TextLine(2:end);
-        SeqStr = '';
-        TextLine = fgetl(FID);
-        while TextLine(1) ~= '>' %Get the sequence
-            SeqStr = cat(2,SeqStr,TextLine);
-            TextLine = fgetl(FID);
-            if isnumeric(TextLine); break; end
-            if isempty(TextLine); break; end
-        end
-        Sequence{j} = strrep(SeqStr,' ','');
-        j = j+1;
-        CurLine = CurLine + 1;
-    elseif isnumeric(TextLine)
-        break;
-    else        
-        TextLine = fgetl(FID);
-    end
-end
+Texts = textscan(FID, '%s', 'delimiter', '\n');
+Texts = Texts{1};
 fclose(FID);
+
+BrakLoc = regexp(Texts, '>', 'once');
+Idx = find(~cellfun('isempty', BrakLoc));
+SeqRange(2) = min([SeqRange(2) length(Idx)]);
+
+Header = cell(diff(SeqRange) + 1, 1);
+Seq = cell(diff(SeqRange) + 1, 1);
+k = 1;
+for j = SeqRange(1):SeqRange(2)
+    Io = Idx(j) + 1;
+    if j < length(Idx)
+        If = Idx(j+1) - 1;
+    else
+        If = length(Texts);
+    end
+    Header{k} = Texts{Idx(j)}(BrakLoc{Idx(j)}+1:end);
+    Seq{k} = strrep(strcat(Texts{Io:If}), ' ', '');
+    k = k+1;
+end
